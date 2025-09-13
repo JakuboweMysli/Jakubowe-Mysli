5d1c818609a736536ae1ef121fc004fc1582c5e5
GB <greenbrowne@protonmail.me>
2025-09-13 23:16:54 +0200

    dzialajace komentarze per wpis

--- netlify/functions/comments.js
+++ netlify/functions/comments.js
@@ -1,34 +1,58 @@
+const fs = require('fs');
 const { google } = require('googleapis');
 
 exports.handler = async function(event) {
-  const auth = new google.auth.GoogleAuth({
-    credentials: JSON.parse(process.env.GOOGLE_SERVICE_ACCOUNT),
-    scopes: ['https://www.googleapis.com/auth/spreadsheets'],
-  });
-  const client = await auth.getClient();
-  const sheets = google.sheets({version: 'v4', auth: client});
-  const spreadsheetId = process.env.SPREADSHEET_ID;
+  try {
+    // wczytanie credentials z pliku
+    const credentials = JSON.parse(fs.readFileSync(process.env.GOOGLE_SERVICE_ACCOUNT_PATH, 'utf8'));
 
-  if (event.httpMethod === 'POST') {
-    const { nick, comment } = JSON.parse(event.body);
-    await sheets.spreadsheets.values.append({
-      spreadsheetId,
-      range: 'Sheet1!A:B',
-      valueInputOption: 'RAW',
-      requestBody: { values: [[nick, comment]] },
+    const auth = new google.auth.GoogleAuth({
+      credentials,
+      scopes: ['https://www.googleapis.com/auth/spreadsheets'],
     });
-    return { statusCode: 200, body: JSON.stringify({ ok: true }) };
-  }
 
-  if (event.httpMethod === 'GET') {
-    const res = await sheets.spreadsheets.values.get({
-      spreadsheetId,
-      range: 'Sheet1!A:B',
-    });
-    const values = res.data.values || [];
-    const comments = values.map(row => ({ nick: row[0], comment: row[1] }));
-    return { statusCode: 200, body: JSON.stringify(comments) };
-  }
+    const client = await auth.getClient();
+    const sheets = google.sheets({ version: 'v4', auth: client });
+    const spreadsheetId = process.env.SPREADSHEET_ID;
 
-  return { statusCode: 405, body: 'Method Not Allowed' };
+    if (event.httpMethod === 'POST') {
+      const { nick, comment, postId } = JSON.parse(event.body);
+      if (!nick || !comment || !postId) {
+        return { statusCode: 400, body: 'Missing nick, comment, or postId' };
+      }
+
+      await sheets.spreadsheets.values.append({
+        spreadsheetId,
+        range: 'Arkusz1!A:C', // kolumny: nick, comment, postId
+        valueInputOption: 'RAW',
+        requestBody: { values: [[nick, comment, postId]] },
+      });
+
+      return { statusCode: 200, body: JSON.stringify({ ok: true }) };
+    }
+
+    if (event.httpMethod === 'GET') {
+      const postId = event.queryStringParameters?.postId;
+      if (!postId) {
+        return { statusCode: 400, body: 'Missing postId query parameter' };
+      }
+
+      const res = await sheets.spreadsheets.values.get({
+        spreadsheetId,
+        range: 'Arkusz1!A:C',
+      });
+
+      const values = res.data.values || [];
+      const comments = values
+        .filter(row => row[2] === postId) // tylko komentarze dla danego wpisu
+        .map(row => ({ nick: row[0], comment: row[1] }));
+
+      return { statusCode: 200, body: JSON.stringify(comments) };
+    }
+
+    return { statusCode: 405, body: 'Method Not Allowed' };
+  } catch (err) {
+    console.error(err);
+    return { statusCode: 500, body: 'Internal Server Error' };
+  }
 };
